a330ba0b503ea5fcfacfa5d0f7138919869a668e
Rajat Malik <37630218+rajatmalik24@users.noreply.github.com>
2024-06-08 16:34:04 +0530

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-publicPath: process.env.NODE_ENV === 'production'
-module.exports = {
- publicPath: process.env.NODE_ENV === 'production'? '/HelloWorld/': '/'
-}
